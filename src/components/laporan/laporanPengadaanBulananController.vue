00d9c56a36a4f8f19e0b3791f2b9358501d6f8ae
joelapoi <55506069+joelapoi@users.noreply.github.com>
2020-05-25 18:25:34 +0800

    laporan Tahunan
    Laporan Pengadaan Tahunan udh fix

---
+++ src/components/laporan/laporanPengadaanBulananController.vue
@@ -0,0 +1,175 @@
+<template>
+<v-container dark>
+    <v-container grid-list-md mb-0>
+        <center>
+            <img src="../kopsurat.png" > 
+        </center>
+        <v-layout row wrap style="margin:10px">
+        <v-dialog v-model="dialogLaporan" max-width="1000px">
+            <v-card>
+                <v-card-text>
+                    <v-container>
+                        <Pdf :src="urlLaporan"></Pdf>
+                    </v-container>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue accent-2" text @click="dialogLaporan = false">Tutup</v-btn>
+                    <v-btn color="green lighten-1" text @click="showLaporan('download')">Download</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+        </v-layout>
+        <v-layout row wrap style="margin:10px">
+            <v-flex xs6>
+                <v-col cols="5">
+                    <h4 class="text-md-left">Pilih tahun</h4> 
+                        <v-select
+                        :items="pilihantahun"
+                        v-model="form.tahun"
+                        >
+                    </v-select>  
+                </v-col> 
+            </v-flex>
+        </v-layout>
+        <v-layout>
+        <v-spacer></v-spacer>
+        <v-btn
+        color="blue accent-2" text
+        @click="showLaporan(form.tahun)"
+        > Tampil Laporan Tahunan </v-btn>
+        </v-layout>
+    </v-container>
+</v-container>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Share+Tech+Mono");
+
+table th {
+    border-left: 1px solid #dddddd;
+    border-right: 1px solid #dddddd;
+}
+
+table td {
+    border-left: 1px solid #dddddd;
+    border-right: 1px solid #dddddd;
+}
+
+tbody tr:nth-of-type(odd) {
+    background-color: rgba(0, 0, 0, .05);
+}
+
+.v-data-table
+    /deep/
+    tbody
+    /deep/
+    tr:hover:not(.v-data-table__expanded__content) {
+        background: #8797a8 !important;
+    }
+    
+.flex {
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+}
+
+.v-select__selections {
+    max-width: 50px;
+    border: none;
+}
+
+.v-select .dropdown-toggle {
+    border: none;
+}
+</style>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import Pdf from 'vue-pdf'
+import { required, min_value } from 'vee-validate/dist/rules'
+    import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
+    setInteractionMode('eager')
+
+    extend('required', {
+        ...required,
+        message: '{_field_} tidak boleh kosong.',
+    })
+    
+export default {
+    components: {
+        Pdf,
+        ValidationProvider,
+        ValidationObserver,
+    },
+    data() {
+        return {
+            pilihantahun:["2008","2014","2019"],
+            pilihanbulan:[],
+            dialogLaporan: false,
+            form: {
+                tahun : '',
+                bulan : '',
+            },
+            tahun : '',
+            urlLaporan : '',
+            errors: '',
+            user: new FormData,
+        }
+    },
+    validations: {
+        form: {
+            tahun: { required },
+        },
+    },
+    methods: {
+        showLaporan(tahun){
+            if(tahun=='download'){
+                window.open(this.$apiUrl + '/laporan/pengadaan/cetak_pdf/'+this.form.tahun, "_blank");
+            }else{
+                this.form.tahun = tahun
+                this.urlLaporan = this.$apiUrl + '/laporan/pengadaan/tampil_pdf/'+tahun
+            }
+            this.dialogLaporan = true;
+        },
+        
+        close() {
+            this.dialog = false
+            this.typeInput = 'Tambah';
+            this.clear()
+        },
+        clear() {
+            this.resetForm();
+        },
+        getRole() {
+            return localStorage.getItem('role');
+        },
+        getUsername() {
+            return localStorage.getItem('username');
+        },
+        initData() {
+        }
+    },
+    watch: {
+        // //KETIKA VALUE BULAN BERUBAH, MAKA KITA REQUEST DATA BARU
+        //     month() {
+        //         this.getLaporan({
+        //             month: this.month,
+        //             year: this.year
+        //         })
+        //     },
+        //     //KETIKA VALUE TAHUN BERUBAH, MAKA KITA REQUEST DATA BARU
+        //     year() {
+        //         this.getLaporan({
+        //             month: this.month,
+        //             year: this.year
+        //         })
+        //     }
+    },
+    mounted() {
+        this.initData()
+    },
+}
+</script>
